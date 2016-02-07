214b2f8c3f9372c03ab164cc1672718c0a476bfb
Aleksey <alieksiei.nikonov.87@mail.ru>
2016-02-07 19:48:43 +0300

    New project of game.ui

---
+++ game1/game1/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game1
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _val = "beleberda!";
+
+        public string Val
+        {
+            get
+            {
+                return _val;
+            }
+            set
+            {
+                _val = value;
+                OnPropertyChanged("Val");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }// make sure only to call this if the value actually changes
+        }
+    }
+}
