e1e4fee3c34675f74e210e10b9679aa452816af4
Aleksey <alieksiei.nikonov.87@mail.ru>
2016-02-15 22:58:28 +0300

    Begin to work (Form1.GeekBrains)!

---
+++ MyUtilitysByGeekBrains/MyUtilitysByGeekBrains/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyUtilitysByGeekBrains
+{
+    public partial class MainForm : Form
+    {
+        int count = 0;
+        Random rnd;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            rnd = new Random();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsmiExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tsmiAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа \"Мои Утилиты\" содержит ряд интересных программ, которые могут пригодиться в жизни, " +
+                "а главное помогут мне в обучении программированию на языке C#!\nАвтор Никонов Алексей Олегович, 29 лет...", "О программе");
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            count++;
+            lblCount.Text = count.ToString();
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            count--;
+            lblCount.Text = count.ToString();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            lblCount.Text = Convert.ToString(count);
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            int n;
+            n = rnd.Next(Convert.ToInt32(numericUpDown1.Value), Convert.ToInt32(numericUpDown2.Value) +1);
+            lblRandom.Text = n.ToString();
+        }
+    }
+}
